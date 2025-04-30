99c4cf1ef6e868ad6799040a3c99c5c79db92ae4
Yusuf Celal <yusufcelal34@gmail.com>
2025-04-30 21:57:58 +0300

    bugun

--- src/day55_maps/C03_YilSonuSinifUpdate.java
+++ src/day55_maps/C03_YilSonuSinifUpdate.java
@@ -1,17 +1,24 @@
 package day55_maps;
 
-public class C03_YilSonuSinifUpdate extends day54_maps.MapDepo {
+import day54_maps.MapDepo;
+
+public class C03_YilSonuSinifUpdate extends MapDepo {
 
     public static void main(String[] args) {
 
         // Yil sonunda calisacak sekilde
         // tum ogrencileri sube ayni kalacak sekilde bir ust sinifa tasiyin
         // Not : 12.siniftakiler ==> mezun olacak
-        // mezun olanlar ayni kalacak
+        //       mezun olanlar ayni kalacak
 
         System.out.println(ogrenciMap);
 
-        yilSOnuSinifArttir();;
+
+        yilSonuSinifArtir();
+        System.out.println(ogrenciMap);
+
+
+        yilSonuSinifArtir();
         System.out.println(ogrenciMap);
 
     }
